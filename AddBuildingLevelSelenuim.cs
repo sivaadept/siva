0383533cc013489d916d949826a23f5fc4d1fb07
siva <>
2017-08-17 15:55:52 +0530

    test

---
+++ AddBuildingLevelSelenuim.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using AutomationFramework.Application.Selenium.Extensions;
+using AutomationFramework.Application.Selenium.Screens;
+using OpenQA.Selenium;
+using WJIV.App.Interfaces;
+
+namespace WJIV.Application.Selenium.Screens
+{
+    public class AddBuildingLevelSelenuim : AbstractSeleniumScreen, AddBuildingLevel
+    {
+        public AddBuildingLevelSelenuim(IWebDriver driver)
+            : base(driver)
+        {
+        }
+
+        public bool IsAddBuildingLevelHeadingDisplayed()
+        {
+            return Driver.FindElement(By.Id("form-info-wj4")).FindElement(By.TagName("h2")).Text.Equals("Add Building Level");
+        }
+
+        public bool IsAllBuildingFieldsDispalyed()
+        {
+            return Driver.FindElement(By.Id("OrgBuildings_0__BuildingName")).Displayed && Driver.FindElement(By.Id("OrgBuildings_1__BuildingName")).Displayed && Driver.FindElement(By.Id("OrgBuildings_2__BuildingName")).Displayed
+                   && Driver.FindElement(By.Id("OrgBuildings_3__BuildingName")).Displayed && Driver.FindElement(By.Id("OrgBuildings_4__BuildingName")).Displayed && Driver.FindElement(By.Id("OrgBuildings_5__BuildingName")).Displayed;
+                
+        }
+
+        public bool IsSaveButtonDisplayed()
+        {
+
+            return Driver.FindElement(By.Id("btnSave")).Displayed;
+        }
+
+        public bool IsSaveAndAddAnotherButtonDisplayed()
+        {
+
+            return Driver.FindElement(By.Id("btnAddSave")).Displayed;
+        }
+
+        public bool IscancelButtonDisplayed()
+        {
+            return IsElementEnabled(By.Name("Cancel"));
+        }
+
+        public bool VerifyMaxCharAllFields()
+        {
+            return Driver.FindElement(By.Id("OrgBuildings_0__BuildingName")).GetAttribute("maxlength") == "100" && Driver.FindElement(By.Id("OrgBuildings_1__BuildingName")).GetAttribute("maxlength") == "100" && Driver.FindElement(By.Id("OrgBuildings_2__BuildingName")).GetAttribute("maxlength") == "100"
+                   && Driver.FindElement(By.Id("OrgBuildings_3__BuildingName")).GetAttribute("maxlength") == "100" && Driver.FindElement(By.Id("OrgBuildings_4__BuildingName")).GetAttribute("maxlength") == "100" && Driver.FindElement(By.Id("OrgBuildings_5__BuildingName")).GetAttribute("maxlength") == "100";
+               
+        }
+
+        public bool VerifyInvalidcharInNameField()
+        {
+            Driver.FindElement(By.Id("OrgBuildings_0__BuildingName")).SendKeys("a@");
+            Driver.FindElement(By.Id("OrgBuildings_1__BuildingName")).Click();
+            Thread.Sleep(100);
+            Driver.FindElement(By.Id("btnSave")).Click();
+            Thread.Sleep(1000);
+            return Driver.FindElement(By.Id("divblerrormsgpopup")).Text.Trim().Equals("Invalid Building Name.");
+
+        }
+
+        public bool VerifyDuplicateBuildingLevelName()
+        {
+            Driver.FindElement(By.Id("OrgBuildings_0__BuildingName")).SendKeys("Build1");
+            Driver.FindElement(By.Id("OrgBuildings_1__BuildingName")).Click();
+            Thread.Sleep(100);
+            Driver.FindElement(By.Id("btnSave")).Click();
+            Thread.Sleep(1000);
+            return Driver.FindElement(By.Id("divblerrormsgpopup")).Text.Trim().Equals("One or more Building Names already exists. Please create a new Building Name to continue.");
+        }
+        public void SaveButton_Click()
+        {
+            Thread.Sleep(1000);
+            Driver.FindElement(By.Id("btnSave")).Click();
+        }
+        
+        public void AddAndSaveAnotherBtnClick()
+        {
+            Driver.FindElement(By.Id("btnAddSave")).Click();
+            Thread.Sleep(10000);
+        }
+
+        public bool VerifyBuildingSucessfullySavedMsg()
+        {
+            return Driver.FindElement(By.Id("divsavebuildingsuccess")).FindElement(By.TagName("h6")).Text.Trim().Equals("Building Level successfully saved.");
+            
+            
+        }
+
+        public bool VerifyCancelYesButton(string url)
+        {
+            Driver.FindElement(By.Name("Cancel")).Click();
+            Driver.FindElements(By.Id("subpopup_delete1")).Where(x => x.GetAttribute("onclick").Equals("Onyesclick()")).First().Click();
+            return Driver.Url.Equals(url);
+        }
+
+        public bool VerifyCancelNoButton()
+        {
+            string AddSubjectPageUrl = Driver.Url;
+            Driver.FindElement(By.Name("Cancel")).Click();
+            Driver.FindElement(By.Id("subpopup_cancel1")).Click();
+            string PostClickUrl = Driver.Url;
+            return AddSubjectPageUrl.Equals(PostClickUrl);
+        }
+
+        public bool VerifyAddAndSaveAnotherButton()
+        {
+            Driver.WaitForElementToBeEnabled(By.Id("btnAddSave"), 30000);
+            Driver.FindElement(By.Id("btnAddSave")).Click();
+            Thread.Sleep(1000);
+            WebElementExtensions.WaitForPageToLoad(Driver);
+            return Driver.FindElement(By.Id("OrgBuildings_0__BuildingName")).GetAttribute("value").Equals("") && Driver.FindElement(By.Id("OrgBuildings_1__BuildingName")).GetAttribute("value").Equals("") && Driver.FindElement(By.Id("OrgBuildings_2__BuildingName")).GetAttribute("value").Equals("")
+                     && Driver.FindElement(By.Id("OrgBuildings_3__BuildingName")).GetAttribute("value").Equals("") && Driver.FindElement(By.Id("OrgBuildings_4__BuildingName")).GetAttribute("value").Equals("") && Driver.FindElement(By.Id("OrgBuildings_5__BuildingName")).GetAttribute("value").Equals("");
+        }
+
+        public void SetBuildingNameField0(string Bname0)
+        {
+            Thread.Sleep(10000);
+            Driver.FindElement(By.Id("OrgBuildings_0__BuildingName")).SendKeys(Bname0);
+        }
+
+        public void SetBuildingNameField1(string Bname1)
+        {
+            Driver.FindElement(By.Id("OrgBuildings_1__BuildingName")).SendKeys(Bname1);
+        }
+
+        public void SetBuildingNameField2(string Bname2)
+        {
+            Driver.FindElement(By.Id("OrgBuildings_2__BuildingName")).SendKeys(Bname2);
+        }
+
+        public void SetBuildingNameField3(string Bname3)
+        {
+            Driver.FindElement(By.Id("OrgBuildings_3__BuildingName")).SendKeys(Bname3);
+        }
+
+        public void SetBuildingNameField4(string Bname4)
+        {
+            Driver.FindElement(By.Id("OrgBuildings_4__BuildingName")).SendKeys(Bname4);
+        }
+
+        public void SetBuildingNameField5(string Bname5)
+        {
+            Driver.FindElement(By.Id("OrgBuildings_5__BuildingName")).SendKeys(Bname5);
+        }
+
+        // Building level Setup
+
+        public bool VerifyBuildingLevelSetupPopup()
+        {
+            return Driver.FindElement(By.Id("BuildinglevelDiv-wmls")).Displayed && Driver.FindElement(By.Id("BuildinglevelDiv-wmls")).FindElement(By.ClassName("h6")).Text.Trim().Contains("Building Level Reporting Setup") &&
+                 Driver.FindElement(By.Id("BuildinglevelDiv-wmls")).FindElement(By.ClassName("pop-up-container")).Text.Trim().Contains("Before you can begin administration of WMLS III, a building level structure must be set up to ensure easy roll-up of reports. Please click Continue to begin adding Building Level Structure.");
+        }
+
+        public void ContinueButton_Click()
+        {
+            Driver.FindElement(By.XPath("html/body/div[20]/div[2]/div[2]/div/div[3]/input")).Click();
+        }
+
+        public bool IsAddBuildingPopupDisplayed()
+        {
+            return Driver.FindElement(By.Id("popupAddBuildings")).Displayed;
+        }
+
+        public bool VerifyAddBuildingLevelPopupHeading()
+        {
+            return Driver.FindElement(By.Id("divblheader")).Text.Trim().Equals("Add Building Level");
+                           
+        }
+
+        public bool IsInstructionsMsgDisplayed()
+        {
+            return Driver.FindElements(By.TagName("b")).Where(x => x.Text.Trim().Equals("Instructions")).First().Displayed;
+            //Driver.FindElement(By.Id("infolevel")).FindElements(By.TagName("div ")).Where(x => x.Text.Trim().Contains("Enter the name of the building(s) you would like to add to your reporting structure.")).First().Displayed;
+        }
+
+        public bool IsBuildingNamesHeadingDispalyed()
+        {
+            return Driver.FindElements(By.TagName("b")).Where(x => x.Text.Trim().Contains("Building Name(s):")).First().Displayed;
+        }
+
+        public bool IsPopupSaveButtonDisplayed()
+        {
+
+            return Driver.FindElement(By.Id("btnSavepartial")).Displayed;
+        }
+
+        public bool IsPopupSaveAndAddAnotherButtonDisplayed()
+        {
+
+            return Driver.FindElement(By.Id("btnAddSavepartial")).Displayed;
+        }
+
+        public bool IsPopupcancelButtonDisplayed()
+        {
+            return IsElementEnabled(By.Id("btnSaveUserCancel"));
+        }
+
+        public bool VerifyPopupInvalidcharInNameField()
+        {
+            Driver.FindElement(By.Id("OrgBuildings_0__BuildingName")).SendKeys("a@");
+            Driver.FindElement(By.Id("OrgBuildings_0__BuildingName")).SendKeys(Keys.Enter);
+            //Driver.FindElement(By.Id("OrgBuildings_1__BuildingName")).Click();
+            Thread.Sleep(100);
+            Driver.FindElement(By.Id("btnSavepartial")).Click();
+            Thread.Sleep(1000);
+            return Driver.FindElement(By.Id("divblerrormsgpopup")).Text.Trim().Equals("Invalid Building Name.");
+
+        }
+
+        public bool VerifyPopupCancelNoButton()
+        {
+            string AddSubjectPageUrl = Driver.Url;
+            Driver.FindElement(By.Id("btnSaveUserCancel")).Click();
+            Driver.FindElement(By.Id("popup_no")).Click();
+            string PostClickUrl = Driver.Url;
+            return AddSubjectPageUrl.Equals(PostClickUrl);
+        }
+
+        public bool VerifyPopupCancelYesButton(string url)
+        {
+            Driver.FindElement(By.Id("btnSaveUserCancel")).Click();
+            Driver.FindElement(By.Id("popup_yes")).Click();
+            return Driver.Url.Equals(url);
+        }
+
+        public void ClickBuildingNameField1()
+        {
+            Driver.FindElement(By.Id("OrgBuildings_1__BuildingName")).Click();
+        }
+
+        public void PopupSaveButton_Click()
+        {
+            Driver.FindElement(By.Id("btnSavepartial")).Click();
+        }
+
+
+       
+        
+
+
+                                                                                                          
+    }
+}
---
+++ AddCaseloadSelenium.cs
@@ -0,0 +1,154 @@
+﻿using System.Linq;
+using System.Threading;
+using AutomationFramework.Application.Selenium.Extensions;
+using AutomationFramework.Application.Selenium.Screens;
+using OpenQA.Selenium;
+using WJIV.App.Interfaces;
+
+namespace WJIV.Application.Selenium.Screens
+{
+    public class AddCaseloadSelenium : AbstractSeleniumScreen, AddCaseload
+    {
+        public AddCaseloadSelenium(IWebDriver driver)
+            : base(driver)
+        {
+        }
+
+        public bool IsCaseloadNameFieldDisplayed()
+        {
+            return IsElementEnabled(By.Id("inpTitle"));
+        }
+
+        public bool VerifyMaxCharCaseloadNameField()
+        
+            return (Driver.FindElement(By.Id("inpTitle")).GetAttribute("maxlength") == "100");
+        
+        public bool VerifySaveButtonDisplayed()
+        
+            return Driver.FindElement(By.Id("btnSave")).Displayed;
+        
+
+        public bool VerifyCaseloadErrorMessage()
+        {
+            Driver.FindElement(By.Id("inpTitle")).Clear();
+            Driver.FindElement(By.Id("btnSave")).Click();
+            return Driver.FindElement(By.Id("spErrName")).Text.Trim().Equals("Caseload folder name required");
+        }
+
+        public bool VerifyPageTitleDisplayed(string title)
+        {
+            return title.Equals(Driver.Title);
+        }
+
+        public void SetCaseloadName(string caseloadName)
+        {
+            Thread.Sleep(6000);
+            WebElementExtensions.WaitForElement(Driver, By.Id("inpTitle"), 30000);
+            Driver.FindElement(By.Id("inpTitle")).SendKeys(caseloadName);
+            Thread.Sleep(2000);
+        }
+
+        public void SaveButton_Click()
+        {
+            WebElementExtensions.WaitForElement(Driver, By.Id("btnSave"));
+            Driver.FindElement(By.Id("btnSave")).Click();
+        }
+
+        public void ClearCaseloadNamefield()
+        {
+            Driver.FindElement(By.Id("inpTitle")).Clear();
+        }
+
+        public bool VerifyPopulateCaseloadName(string caseloadName)
+        {
+            var CaseloadName = Driver.FindElement(By.ClassName("search-results")).FindElements(By.TagName("td"))[1].Text;
+            return caseloadName.Equals(CaseloadName);
+        }
+
+        public bool IsCaseloadActionIconsDisplayed()
+        {
+            return (Driver.FindElement(By.ClassName("subject-edit")).Displayed
+                && Driver.FindElement(By.ClassName("subject-Delete")).Displayed);
+        }
+
+        public bool IsCaseloadActionIconsEnabled()
+        {
+            var caseloadName = Driver.FindElement(By.ClassName("search-results")).FindElements(By.TagName("td"))[3];
+            return (caseloadName.FindElement(By.ClassName("subject-edit")).Enabled
+                && caseloadName.FindElement(By.ClassName("subject-Delete")).Enabled);
+        }
+
+        public bool IsFooterDisplayed()
+        {
+            var checkboxes = Driver.FindElements(By.CssSelector("input[type='checkbox']"));
+
+            if (checkboxes.Count > 2)
+            {
+                for (int i = 1; i < 3; i++)
+                    Driver.FindElements(By.CssSelector("input[type='checkbox']"))[i].Click();
+            }
+
+            return (Driver.FindElement(By.Id("thSelectedItems")).Text.Trim().Equals("With Selected Items")
+                && Driver.FindElement(By.ClassName("subject-Delete")).Displayed);
+        }
+
+        public bool IsActionIconsGrayedOut()
+        {
+
+            bool bool_value;
+            bool_value = false
+            
+            var checkboxes = Driver.FindElements(By.CssSelector("input[type='checkbox']"));
+
+            if (checkboxes.Count > 2)
+            {
+                for (int i = 1; i < checkboxes.Count; i++)
+                {
+                    bool_value = (Driver.FindElements(By.TagName("tr"))[i].FindElements(By.TagName("td"))[3].FindElements(By.TagName("a"))[0].GetAttribute("class").Trim().Equals("subject-edit-gray"))
+                                    && (Driver.FindElements(By.TagName("tr"))[i].FindElements(By.TagName("td"))[3].FindElements(By.TagName("a"))[1].GetAttribute("class").Trim().Equals("caseload-Delete-gray"));
+                    if (bool_value == true) 
+                    {
+                        j++;
+                        if (j == 2)
+                            break;
+
+                    }
+                        
+                
+            }
+            return bool_value;
+        }
+
+        public bool Only25RecordsPerPageAreDisplayed()
+        {
+            return Driver.FindElement(By.Id("tbdyCaseFolders")).FindElements(By.TagName("tr")).Count().Equals(25);
+        }
+
+        public bool VerifyPagination()
+        {
+            return Driver.FindElement(By.ClassName("pagination")).FindElements(By.TagName("li")).Where(x => x.Text.Contains("First")).First().Displayed;
+        }
+
+        public bool VerifyDuplicateCaseloadErrorMessage(string caseloadName)
+        {
+            Driver.FindElement(By.Id("inpTitle")).SendKeys(caseloadName);
+            Driver.FindElement(By.Id("btnSave")).Click();
+            return Driver.FindElement(By.Id("spErrName")).Text.Trim().Equals("Caseload folder name already exists");
+        
+        public bool VerifyDuplicateErrorMessage()
+        {
+            Driver.WaitForPageToLoad();
+            return Driver.FindElement(By.ClassName("field-validation-error-caseload")).Text.Contains("Caseload folder name already exists");
+        }
+        public bool VerifySelectAllFunctionality()
+        {
+            Driver.FindElement(By.Id("chbCaseFolders")).Click();
+            Driver.FindElements(By.ClassName("selectedId"))[1].Click();
+            Driver.FindElements(By.ClassName("selectedId"))[1].Click();
+            bool result= Driver.FindElement(By.Id("chbCaseFolders")).Selected;
+            Driver.FindElement(By.Id("chbCaseFolders")).Click();
+            return result;
+        
+    }
+}
+
